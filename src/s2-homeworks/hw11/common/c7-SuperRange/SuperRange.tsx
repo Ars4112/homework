edec4df9069d21444752ae8f6396cc639a06d01b
Арс <arsenijlipin@MacBook-Pro-Vera.local>
2024-09-01 20:39:26 +0300

    homeWork-11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,59 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React from "react";
+import { Slider, SliderProps, styled } from "@mui/material";
+
+const SliderStyled = styled(Slider)(({ theme }) => ({
+	color: "#00CC22",
+	height: 3,
+    width: 147,
+	padding: "13px 0",
+	"& .MuiSlider-thumb": {
+		height: 18,
+		width: 18,
+		backgroundColor: "#fff",
+		border: "1px solid currentColor",
+
+		"&:hover": {
+			boxShadow: "0 0 0 8px rgba(58, 133, 137, 0.16)",
+		},
+		"&::before": {
+			height: 6,
+			width: 6,
+            backgroundColor: "currentColor"
+		},
+		"& .airbnb-bar": {
+			height: 9,
+			width: 1,
+			backgroundColor: "currentColor",
+			marginLeft: 1,
+			marginRight: 1,
+		},
+	},
+	"& .MuiSlider-track": {
+		height: 3,
+	},
+	"& .MuiSlider-rail": {
+		color: "#8B8B8B",
+		opacity: 1,
+		height: 3,
+		...theme.applyStyles("dark", {
+			color: "#bfbfbf",
+			opacity: undefined,
+		}),
+	},
+}));
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
-}
+	return (
+		<SliderStyled
+			value={props.value}
+			onChange={props.onChange}
+			sx={{
+				// стили для слайдера // пишет студент
+				
+			}}
+			{...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+		/>
+	);
+};
 
-export default SuperRange
+export default SuperRange;
