24842eb153f0497d90f38e22b1007941a2c4cbd0
Арс <arsenijlipin@MacBook-Pro-Vera.local>
2024-09-04 19:01:50 +0300

    Homework-12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,25 @@
 const initState = {
-    themeId: 1,
-}
+	themeId: 1,
+};
+type StateType = typeof initState;
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (
+	state = initState,
+	action: ActionType
+): StateType => {
+	// fix any
+	switch (action.type) {
 
-        default:
-            return state
-    }
-}
+		// дописать
+		case "SET_THEME_ID": {
+			return { ...state, themeId: action.id };
+		}
+		default:
+			return state;
+	}
+};
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionType = ReturnType<typeof changeThemeId>;
+
+export const changeThemeId = (id: number) =>
+	({ type: "SET_THEME_ID", id } as const); // fix any
